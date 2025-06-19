cdee80712b921a53fa396dd4b811746d2a9f7254
Gautam Mishra <njoygautam12@gmail.com>
2025-06-19 19:52:12 +0530

    Refactor MainPageScreen to use StatefulWidget and implement TabBar for navigation

--- lib/presentation/main/pages/main_page.dart
+++ lib/presentation/main/pages/main_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:soptify/common/helpers/is_dark_mode.dart';
 import 'package:soptify/common/widgets/appBar/app_bar.dart';
 import 'package:soptify/core/config/assets/app_images.dart';
 import 'package:soptify/core/config/assets/app_vectors.dart';
+import 'package:soptify/core/config/theme/app_color.dart';
 
-class MainPageScreen extends StatelessWidget {
+class MainPageScreen extends StatefulWidget {
   const MainPageScreen({super.key});
+
+  @override
+  State<MainPageScreen> createState() => _MainPageScreenState();
+}
+
+class _MainPageScreenState extends State<MainPageScreen> with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +37,7 @@ class MainPageScreen extends StatelessWidget {
           children: [
              SizedBox(height: 100),
             _homeArtistTopCard(),
+            _tabOptions(),
           ],
         ),
       )
@@ -48,4 +65,33 @@ class MainPageScreen extends StatelessWidget {
      )
     );
   }
+
+  Widget _tabOptions() {
+  return Material(
+    color: Colors.transparent,
+    elevation: 0,
+    shadowColor: Colors.transparent,
+    surfaceTintColor: Colors.transparent,
+    child: TabBar(
+      controller: _tabController,
+      labelColor: context.isDarkMode ? Colors.white : Colors.black,
+      indicatorColor: AppColors.primary, // Optional if you don't want any line
+      dividerColor: Colors.transparent, // THIS removes the bottom line
+      padding: EdgeInsets.symmetric(vertical:20, horizontal: 16),
+      tabAlignment: TabAlignment.center,
+      isScrollable: true,
+      labelStyle: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        color: Color(0xFFDADADA),
+      ),
+      tabs: [
+        Tab(child: Text('News')),
+        Tab(child: Text('Video')),
+        Tab(child: Text('Artists')),
+        Tab(child: Text('Podcasts')),
+      ],
+    ),
+  );
+}
 }
